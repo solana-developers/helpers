f76d0cd3bd7a3ff95b308f4aade8a837a94a6d99
Mike MacCana <mike.maccana@gmail.com>
2024-05-24 11:10:21 -0400

    Add more tests for getExplorerLink()

--- src/index.test.ts
+++ src/index.test.ts
@@ -321,6 +321,11 @@ describe("getExplorerLink", () => {
     assert.equal(link, "https://explorer.solana.com/block/242233124");
   });
 
+  test("getExplorerLink works for a block on mainnet when no network is supplied", () => {
+    const link = getExplorerLink("block", "242233124");
+    assert.equal(link, "https://explorer.solana.com/block/242233124");
+  });
+
   test("getExplorerLink works for an address on mainnet", () => {
     const link = getExplorerLink(
       "address",
@@ -360,6 +365,18 @@ describe("getExplorerLink", () => {
   test("getExplorerLink works for a block on mainnet", () => {
     const link = getExplorerLink("block", "241889720", "mainnet-beta");
     assert.equal(link, "https://explorer.solana.com/block/241889720");
+  });
+
+  test("getExplorerLink provides a localnet URL", () => {
+    const link = getExplorerLink(
+      "tx",
+      "2QC8BkDVZgaPHUXG9HuPw7aE5d6kN5DTRXLe2inT1NzurkYTCFhraSEo883CPNe18BZ2peJC1x1nojZ5Jmhs94pL",
+      "localnet",
+    );
+    assert.equal(
+      link,
+      "https://explorer.solana.com/tx/2QC8BkDVZgaPHUXG9HuPw7aE5d6kN5DTRXLe2inT1NzurkYTCFhraSEo883CPNe18BZ2peJC1x1nojZ5Jmhs94pL?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899",
+    );
   });
 });
 
