ad60467004dd1ecff02bb8df250ceaacd89b6b35
Ayush <ayush-15@live.com>
2024-08-27 03:46:17 +0530

    added changes

--- jest.config.js
+++ jest.config.js
@@ -12,4 +12,5 @@ export default {
       }
     }],
   },
+  testTimeout: 1200000
 };
--- tests/bankrun_test/tests/bankrun.test.ts
+++ tests/bankrun_test/tests/bankrun.test.ts
@@ -1,16 +1,16 @@
-// @ts-nocheck
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { BankrunTest } from "../target/types/bankrun_test";
-import { startAnchor } from "solana-bankrun";
+import { BanksClient, startAnchor } from "solana-bankrun";
 import { BankrunProvider } from "anchor-bankrun";
 import {
   PublicKey,
   sendAndConfirmTransaction,
   Transaction,
 } from "@solana/web3.js";
-import { BankrunContextWrapper } from "../../../src/lib/bankrun-context";
-import assert from "node:assert";
+import { BankrunContextWrapper } from "../../../src/index";
+import { assert } from "chai";
+import { describe, it } from "node:test";
 
 const IDL = require("../target/idl/bankrun_test.json");
 const PROGRAM_ID = new PublicKey(IDL.address);
@@ -22,16 +22,30 @@ describe("bankrun_test", async () => {
     []
   );
   const provider = new BankrunProvider(context);
-  const payer = provider.wallet as anchor.Wallet;
+  const wallet = provider.wallet as anchor.Wallet;
   const bankrunContextWrapper = new BankrunContextWrapper(context);
   const connection = bankrunContextWrapper.connection.toConnection();
   const program = new anchor.Program<BankrunTest>(IDL, provider);
+  const client = context.banksClient;
 
   it("Is initialized!", async () => {
     const ix = await program.methods.initialize().instruction();
     const tx = new Transaction().add(ix);
-    const { blockhash, _height } = await connection.getLatestBlockhash();
-    const sig = await sendAndConfirmTransaction(connection, tx);
-    assert.ok(sig);
+    const { blockhash, lastValidBlockHeight } =
+      await connection.getLatestBlockhash();
+    const sig = await sendAndConfirmTransaction(connection, tx, [wallet.payer]);
+    const status = await client.getTransactionStatus(sig);
+
+    assert(typeof blockhash === "string", "blockhash is not a string");
+    assert(
+      typeof lastValidBlockHeight === "number",
+      "lastValidBlockHeight is not a number"
+    );
+    assert(
+      typeof sig === "string",
+      "Signature from transaction is not a string"
+    );
+    console.log()
+    assert.ok(status.confirmationStatus !== "confirmed", "Txn wasn't confirmed");
   });
 });
