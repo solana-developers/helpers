3e43a78eeac1e734ef6ef14ff8f3ae967d04ac35
Ayush <ayush-15@live.com>
2024-08-29 02:56:54 +0530

    added jest for testing

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+  testEnvironment: "node",
+  preset: 'ts-jest',
+  testMatch: ['**/tests/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/bankrun_test/'],
+  transform: {
+    '^.+\\.(ts|tsx)?$': ["ts-jest",{
+      tsconfig: 'tsconfig.test.json',
+      diagnostics: {
+        exclude: ['tests/bankrun_test/**'],
+      }
+    }],
+  },
+  testTimeout: 1200000
+};
